4e6cf82a370d428802cfd2face387337f4b69b4e
mpliang02 <mpliang@mit.edu>
2022-01-19 19:28:44 -0500

    added 10 recipes to database

--- server/server.js
+++ server/server.js
@@ -35,8 +35,10 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 const mongoConnectionURL =
-  "mongodb+srv://admin:iYkImZrhSq4DSS0V@cluster0.bnbly.mongodb.net/homemadetest?retryWrites=true&w=majority";
-const databaseName = "homemadetest";
+  "mongodb+srv://mpliang:webDevIsPain890!@cluster0.y0aii.mongodb.net/recipesDB?retryWrites=true&w=majority";
+// "mongodb+srv://admin:iYkImZrhSq4DSS0V@cluster0.bnbly.mongodb.net/homemadetest?retryWrites=true&w=majority";
+// const databaseName = "homemadetest";
+const databaseName = "recipesDB";
 
 // connect to mongodb
 mongoose
@@ -55,7 +57,7 @@ const RecipeSchema = new mongoose.Schema({
 
 const Recipe = mongoose.model("Recipe", RecipeSchema);
 
-let scallion_pancakes = new Recipe({
+let scallionPancakes = new Recipe({
   dishName: "Scallion Pancakes",
   ingredients: ["scallions", "flour", "water", "salt", "oil"],
 });
@@ -79,9 +81,81 @@ let pizza = new Recipe({
   ingredients: ["flour", "salt", "yeast", "cheese", "tomatoes", "sugar", "basil"],
 });
 
-scallion_pancakes.save().then((dish) => console.log("Added ${dish.dishName}"));
-dumplings.save().then((dish) => console.log("Added ${dish.dishName}"));
-pizza.save().then((dish) => console.log("Added ${dish.dishName}"));
+let ramen = new Recipe({
+  dishName: "Ramen",
+  ingredients: ["broth", "noodles", "pork", "bamboo", "eggs", "scallions", "seaweed"],
+});
+
+let chickenSandwich = new Recipe({
+  dishName: "Chicken Sandwich",
+  ingredients: ["bread", "chicken", "tomatoes", "lettuce", "pickles", "mayonnaise"],
+});
+
+let applePie = new Recipe({
+  dishName: "Apple Pie",
+  ingredients: ["flour", "salt", "sugar", "apples", "cinnamon", "butter", "nutmeg"],
+});
+
+let cheesecake = new Recipe({
+  dishName: "Cheesecake",
+  ingredients: [
+    "graham crackers",
+    "sugar",
+    "butter",
+    "cream cheese",
+    "eggs",
+    "lemon juice",
+    "sour cream",
+    "salt",
+  ],
+});
+
+let falafel = new Recipe({
+  dishName: "Falafel",
+  ingredients: [
+    "chickpeas",
+    "onions",
+    "garlic",
+    "parsley",
+    "cilantro",
+    "cumin",
+    "salt",
+    "cardamom",
+    "pepper",
+  ],
+});
+
+let burgers = new Recipe({
+  dishName: "Burgers",
+  ingredients: [
+    "bread",
+    "beef",
+    "salt",
+    "pepper",
+    "cheese",
+    "tomatoes",
+    "lettuce",
+    "onions",
+    "ketchup",
+    "mayonnaise",
+  ],
+});
+
+let donuts = new Recipe({
+  dishName: "Donuts",
+  ingredients: ["flour", "salt", "sugar", "yeast", "milk", "eggs", "butter", "glaze"],
+});
+
+scallionPancakes.save().then((dish) => console.log(`Added ${dish.dishName}`));
+dumplings.save().then((dish) => console.log(`Added ${dish.dishName}`));
+pizza.save().then((dish) => console.log(`Added ${dish.dishName}`));
+ramen.save().then((dish) => console.log(`Added ${dish.dishName}`));
+chickenSandwich.save().then((dish) => console.log(`Added ${dish.dishName}`));
+applePie.save().then((dish) => console.log(`Added ${dish.dishName}`));
+cheesecake.save().then((dish) => console.log(`Added ${dish.dishName}`));
+falafel.save().then((dish) => console.log(`Added ${dish.dishName}`));
+burgers.save().then((dish) => console.log(`Added ${dish.dishName}`));
+donuts.save().then((dish) => console.log(`Added ${dish.dishName}`));
 
 // create a new express server
 const app = express();
