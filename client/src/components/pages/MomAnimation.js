3a6e2b00a354bb4085e5f9429227b8b614aa2282
mpliang02 <mpliang@mit.edu>
2022-01-26 11:43:02 -0500

    readded css things

--- client/src/components/pages/MomAnimation.js
+++ client/src/components/pages/MomAnimation.js
@@ -5,8 +5,10 @@ import mom1 from "../../../dist/characters/mom1.png";
 import mom2 from "../../../dist/characters/mom2.png";
 import grave from "../../../dist/characters/grave.png";
 import ReactAudioPlayer from "react-audio-player";
+import sickMom from "../../../dist/extras/momhead.png";
+import DialogueBox from "./MomDialogueTextBox.js";
 
-const Mom = () => {
+const Mom = ({ firstName, recipe }) => {
   const [imageNumber, setImageNumber] = useState(0);
 
   useEffect(() => {
@@ -22,8 +24,19 @@ const Mom = () => {
 
   const imageArray = [mom1, mom2];
 
+  const messages = [
+    { type: "normal", text: `Oh, hello ${firstName}!`, nextLine: 1 },
+    {
+      type: "normal",
+      text: `You made ${recipe} for me? Thank you!`,
+      nextLine: 1,
+    },
+    { type: "normal", text: `${firstName}...remember...I love you!`, nextLine: 1 },
+  ];
+
   return (
     <>
+      {/* <DialogueBox messages={messages} characterName={"Mom"} dialogueImage={sickMom}></DialogueBox> */}
       <img className="fadeInOut" src={imageArray[imageNumber]}></img>
       <img className="fadeInTransition" src={grave}></img>
       <div className="fadeInOutFirst">special thanks to:</div>
